eacb9114f2ce3c3d40d32be66588f40fe913da69
Federico Scarpa <fedescarpa@gmail.com>
2014-10-13 15:42:45 -0300

    Adding gulp jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 'use strict';
 
 var gulp = require('gulp');
+var sequence = require('run-sequence');
 
 gulp.task('mocha', require('./gulp_tasks/gulp-mocha'));
+
+gulp.task('jshint', require('./gulp_tasks/gulp-jshint'));
+
+gulp.task('test', function (done) {
+  sequence('jshint', 'mocha', done);
+});
